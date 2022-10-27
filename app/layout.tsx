69223d818b7c7481bbb1d1ecd6ba146f5c8456a8
Filipe Veronezi <filipeseidi@hotmail.com>
2022-10-27 20:32:48 -0300

    feat: create main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,21 @@
+import { Header } from '@/components/Header'
 import { Space_Grotesk } from '@next/font/google'
 import './globals.css'
 
 const spaceGrotesk = Space_Grotesk()
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={spaceGrotesk.className}>
+    <html lang="en" className={`text-black ${spaceGrotesk.className}`}>
       <head>
         <title>Trinitas</title>
-        <meta
-          name="description"
-          content="Um curso aberto de teologia para cristãos interessados em crescer em conhecimento bíblico e teológico e em servir à igreja local"
-        />
+        <meta name="description" content="Um curso aberto de teologia para cristãos interessados em crescer em conhecimento bíblico e teológico e em servir à igreja local" />
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
