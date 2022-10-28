ac115868060907cd4dea7f1d6c67d05296ffbd28
Filipe Veronezi <filipeseidi@hotmail.com>
2022-10-27 21:56:43 -0300

    refactor: button props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,18 @@
-interface ButtonProps {
-  children: React.ReactNode
+import { ButtonHTMLAttributes } from 'react'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   color: 'white' | 'black'
 }
 
 const colorClassNames = {
-  white: 'bg-white text-black border-black hover:bg-gray-50',
-  black: 'bg-black text-white border-white hover:bg-gray-800'
+  white: 'bg-white text-black border-black shadow-lg hover:drop-shadow-[4px_4px_0_rgba(32,32,32,1)]',
+  black: 'bg-black text-white border-white hover:drop-shadow-[4px_4px_0_rgba(255,255,255,1)]'
 }
 
-export function Button(props: ButtonProps) {
-  return <button className={`${colorClassNames[props.color]} flex items-center justify-center text-2xl border-[3px] rounded-xl px-10 py-2 transition-colors`}>{props.children}</button>
+export function Button({ color, ...props }: ButtonProps) {
+  return (
+    <button {...props} className={`${colorClassNames[color]} flex items-center justify-center rounded-xl border-[3px] px-10 py-3 text-2xl hover:translate-y-[-4px] hover:translate-x-[-4px] active:translate-x-0 active:translate-y-0 active:drop-shadow-none`}>
+      {props.children}
+    </button>
+  )
 }
