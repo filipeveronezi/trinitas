50f05ff46e91b60fe1db90af85c71c2c419c961c
Filipe Veronezi <filipeseidi@hotmail.com>
2022-10-27 21:39:29 -0300

    chore: setup linting tools for tailwindcss

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'always',
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  semi: false,
+  printWidth: 1000,
+  plugins: [require('prettier-plugin-tailwindcss')]
+}
