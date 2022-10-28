aa502a21b101e659fe8143b2e0286b17cfa5033c
Filipe Veronezi <filipeseidi@hotmail.com>
2022-10-27 21:57:09 -0300

    refactor: reorder tailwindcss class names with prettier

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 
 export function Header() {
   return (
-    <header className="max-w-screen-xl mx-auto px-10 py-8">
+    <header className="mx-auto max-w-screen-xl px-10 py-8">
       <Link href="/">
-        <div className="relative w-[170px] h-[40px] cursor-pointer">
+        <div className="relative h-[40px] w-[170px] cursor-pointer">
           <Image src="/trinitas-horizontal.svg" alt="Logotipo Trinitas" fill />
         </div>
       </Link>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 export function Hero() {
   return (
     <section className="flex flex-col items-center justify-center gap-5">
-      <h1 className="font-bold text-8xl mt-16">Estude as Escrituras</h1>
-      <p className="font-normal text-2xl text-gray-500 max-w-[700px] text-center">Somos um curso aberto de teologia para cristãos interessados em crescer em conhecimento bíblico e teológico e em servir à igreja local</p>
+      <h1 className="mt-16 text-8xl font-bold">Estude as Escrituras</h1>
+      <p className="max-w-[700px] text-center text-2xl font-normal text-gray-600">Somos um curso aberto de teologia para cristãos interessados em crescer em conhecimento bíblico e teológico e em servir à igreja local</p>
       <Button color="white">Garanta sua vaga</Button>
-      <div className="relative mt-10 w-screen h-[400px]">
+      <div className="relative mt-10 h-[400px] w-screen">
         <Image src="/hero-illustration.svg" alt="Desenho de uma mesa de estudos" fill />
       </div>
     </section>
