26dce9d3535a7315cfeb19b254deffd5a516ede5
Filipe Veronezi <filipeseidi@hotmail.com>
2022-10-29 02:25:31 -0300

    feat: add smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body>
+      <body className="scroll-smooth">
         <Header />
         {children}
       </body>
