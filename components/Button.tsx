5b82499ea46bc95caa6b4195e7587db19c0c4e1b
Filipe Veronezi <filipeseidi@hotmail.com>
2022-10-29 10:55:13 -0300

    feat: better button styling on mobile

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,13 +5,13 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const colorClassNames = {
-  white: 'bg-white text-black border-black shadow-lg lg:hover:drop-shadow-[4px_4px_0_rgba(32,32,32,1)]',
-  black: 'bg-black text-white border-white lg:hover:drop-shadow-[4px_4px_0_rgba(255,255,255,1)]'
+  white: 'bg-white text-black border-black shadow-lg drop-shadow-[4px_4px_0_rgba(32,32,32,1)] lg:drop-shadow-none lg:hover:drop-shadow-[4px_4px_0_rgba(32,32,32,1)]',
+  black: 'bg-black text-white border-white drop-shadow-[4px_4px_0_rgba(255,255,255,1)] lg:drop-shadow-none lg:hover:drop-shadow-[4px_4px_0_rgba(255,255,255,1)]'
 }
 
 export function Button({ color, ...props }: ButtonProps) {
   return (
-    <button {...props} className={`${colorClassNames[color]} flex items-center justify-center rounded-xl border-[3px] px-10 py-3 text-xl lg:text-2xl lg:hover:translate-y-[-4px] lg:hover:translate-x-[-4px] lg:active:translate-x-0 lg:active:translate-y-0 lg:active:drop-shadow-none`}>
+    <button {...props} className={`${colorClassNames[color]} flex translate-y-[-4px] translate-x-[-4px] items-center justify-center rounded-xl border-[3px] px-10 py-3 text-xl active:translate-x-0 active:translate-y-0 active:drop-shadow-none lg:translate-x-0 lg:translate-y-0 lg:text-2xl lg:hover:translate-y-[-4px] lg:hover:translate-x-[-4px]`}>
       {props.children}
     </button>
   )
