f1e23ac90a3431b7996ccd6ff95506ea73d97dfd
Filipe Veronezi <filipeseidi@hotmail.com>
2022-10-29 14:52:03 -0300

    feat: add "soon" disclaimer on buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,9 +9,15 @@ const colorClassNames = {
   black: 'bg-black text-white border-white drop-shadow-[4px_4px_0_rgba(255,255,255,1)] lg:drop-shadow-none lg:hover:drop-shadow-[4px_4px_0_rgba(255,255,255,1)]'
 }
 
+const disclaimerColorClassNames = {
+  white: 'bg-black text-white',
+  black: 'bg-white text-black'
+}
+
 export function Button({ color, ...props }: ButtonProps) {
   return (
-    <button {...props} className={`${colorClassNames[color]} flex translate-y-[-4px] translate-x-[-4px] items-center justify-center rounded-xl border-[3px] px-10 py-3 text-xl active:translate-x-0 active:translate-y-0 active:drop-shadow-none lg:translate-x-0 lg:translate-y-0 lg:text-2xl lg:hover:translate-y-[-4px] lg:hover:translate-x-[-4px] lg:active:translate-x-0 lg:active:translate-y-0 lg:active:drop-shadow-none`} type="button">
+    <button {...props} className={`${colorClassNames[color]} relative flex translate-y-[-4px] translate-x-[-4px] cursor-not-allowed items-center justify-center rounded-xl border-[3px] px-10 py-3 text-xl active:translate-x-0 active:translate-y-0 active:drop-shadow-none lg:translate-x-0 lg:translate-y-0 lg:text-2xl lg:hover:translate-y-[-4px] lg:hover:translate-x-[-4px] lg:active:translate-x-0 lg:active:translate-y-0 lg:active:drop-shadow-none`} type="button">
+      <span className={`${disclaimerColorClassNames[color]} absolute -top-3 -left-2 flex h-6 w-20 items-center justify-center rounded-md text-xs font-bold`}>Em breve</span>
       {props.children}
     </button>
   )
