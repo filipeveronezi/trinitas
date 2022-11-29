fccc6ff0bcae1eb11bd4816707bd7268ce7e6bbd
Filipe Veronezi <filipeseidi@hotmail.com>
2022-11-29 11:13:44 -0300

    feat: add lang attribute

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   experimental: {
     fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }]
+  },
+  i18n: {
+    locales: ['pt-BR'],
+    defaultLocale: 'pt-BR'
   }
 }
 
