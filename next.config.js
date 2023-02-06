b4273f9fba1e8be0cdb352a56fafbf59fcc3933f
Filipe Veronezi <filipeseidi@hotmail.com>
2023-02-06 12:10:55 -0300

    chore: bump next and next font

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }]
-  },
   i18n: {
     locales: ['pt-BR'],
     defaultLocale: 'pt-BR'
