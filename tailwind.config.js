edab36f363f2a4eeb65b3877e7357c2d9ba9629c
Filipe Veronezi <filipeseidi@hotmail.com>
2025-02-13 22:11:09 -0300

    chore: update Tailwind content config for Next.js app directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {
       colors: {
